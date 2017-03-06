c04ad3165a864d05def1ae2f85130e2b16520955
marciosmelo <marciomelo@gmail.com>
2017-03-06 15:59:14 -0300

    adicionando sprite do player

--- state0.js
+++ state0.js
@@ -1,15 +1,41 @@
 var demo = {};
+var centerX = 1500/2;
+var centerY = 1000/2;
+var adam, speed = 4;
 demo.state0 = function(){};
 demo.state0.prototype = {
-    preload: function(){},
+    preload: function(){
+        game.load.image('adam', 'assets/sprites/adam.png');
+    },
     create: function(){
         game.stage.backgroundColor = '#80ff80';
         console.log('state0');
         addChangeStateEventListeners();
         game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+        
+        adam = game.add.sprite(centerX, centerY, 'adam');
+        adam.anchor.setTo(0.5,0.5);
        
     },
-    update: function(){}
+    update: function(){
+        if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT))
+        {
+            adam.x += speed;
+        }
+        else if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT))
+        {
+            adam.x -= speed;
+        }
+        if (game.input.keyboard.isDown(Phaser.Keyboard.UP))
+        {
+            adam.y -= speed;
+        }
+        else if (game.input.keyboard.isDown(Phaser.Keyboard.DOWN))
+        {
+            adam.y += speed;
+        }
+    
+    }
 };
 
 function changeState(i, stateNum){
